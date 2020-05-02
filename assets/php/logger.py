b6bae35118f17129dc71a2cb8f32df1beb7954f4
nolimitcarter <carterthayer@protonmail.com>
2020-05-02 02:43:33 -0400

    alias commit

--- assets/php/logger.py
+++ assets/php/logger.py
@@ -1,30 +1,10 @@
 <?php
-
-function logIP()
-{
-             $ipLog="ips.txt"; // Your logfiles name here (.txt or .html extensions ok)
-
-                  // IP logging function by Dave Lauderdale
-                       // Originally published at: www.digi-dl.com
-
-                            $register_globals = (bool) ini_get('register_gobals');
-                                 if ($register_globals) $ip = getenv(REMOTE_ADDR);
-                                      else $ip = $_SERVER['REMOTE_ADDR'];
-
-                                           $date=date ("l dS of F Y h:i:s A");
-                                                $log=fopen("$ipLog", "a+");
-
-                                                     if (preg_match("/\\bhtm\\b/i", $ipLog) || preg_match("/\\bhtml\\b/i", $ipLog))
-                                                          {
-                                                                        fputs($log, "Logged IP address: $ip - Date logged: $date<br>");
-                                                                             }
-                                                               else fputs($log, "Logged IP address: $ip - Date logged: $date\
-                                                                       ");
-
-                                                                    fclose($log);
-                                                                    }
-// Place the below function call wherever you want the script to fire.
-logIp();
-
-
+$logfile= 'info.html';
+$IP = $_SERVER['REMOTE_ADDR'];
+$logdetails=  date("F j, Y, g:i a") . ': ' . '<a target=_blank href=https://www.dnsstuff.com/tools/ipall.ch?domain='.$_SERVER['REMOTE_ADDR'].'>'.$_SERVER['REMOTE_ADDR'].'</a>';
+$rc = fopen($logfile, "a"); 
+fwrite($rc, $logdetails);
+fwrite($rc, "<br />_____ Provided by JavaScripthost.com _____");
+fwrite($rc, "<br>");
+fclose($rc); 
 ?>
