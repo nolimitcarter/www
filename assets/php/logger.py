31e47035c44e4927a1ebecf577da5d1688c5db7c
nolimitcarter <carterthayer@protonmail.com>
2020-05-02 02:39:55 -0400

    alias commit

--- assets/php/logger.py
+++ assets/php/logger.py
@@ -1,10 +1,30 @@
-<?php 
+<?php
 
-$ip = $SERVER['REMOTE_ADDR'];
+function logIP()
+{
+             $ipLog="ips.txt"; // Your logfiles name here (.txt or .html extensions ok)
 
-$file = "ips.txt" 
-$text = gile_get_contents($file);
-$text .= $ip . "\n"; 
-file_put_contents($file, $text);
+                  // IP logging function by Dave Lauderdale
+                       // Originally published at: www.digi-dl.com
+
+                            $register_globals = (bool) ini_get('register_gobals');
+                                 if ($register_globals) $ip = getenv(REMOTE_ADDR);
+                                      else $ip = $_SERVER['REMOTE_ADDR'];
+
+                                           $date=date ("l dS of F Y h:i:s A");
+                                                $log=fopen("$ipLog", "a+");
+
+                                                     if (preg_match("/\\bhtm\\b/i", $ipLog) || preg_match("/\\bhtml\\b/i", $ipLog))
+                                                          {
+                                                                        fputs($log, "Logged IP address: $ip - Date logged: $date<br>");
+                                                                             }
+                                                               else fputs($log, "Logged IP address: $ip - Date logged: $date\
+                                                                       ");
+
+                                                                    fclose($log);
+                                                                    }
+// Place the below function call wherever you want the script to fire.
+logIp();
+
 
 ?>
