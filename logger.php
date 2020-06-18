cd1c51a101e8390206e7b3e12175e0ff3ef3d158
nolimitcarter <carterthayer@protonmail.com>
2020-06-17 22:42:47 -0400

    logger added

--- logger.php
+++ logger.php
@@ -1,11 +1,36 @@
 <?php
-$iplogfile = 'logs/ip-address-mainsite.html';
-$ipaddress = $_SERVER['REMOTE_ADDR'];
-$webpage = $_SERVER['SCRIPT_NAME'];
-$timestamp = date('d/m/Y h:i:s');
-$browser = $_SERVER['HTTP_USER_AGENT'];
-$fp = fopen($iplogfile, 'a+');
-chmod($iplogfile, 0777);
-fwrite($fp, '['.$timestamp.']: '.$ipaddress.' '.$webpage.' '.$browser. "\n<br><br>");
-fclose($fp);
+
+function getUserIP()
+{
+    // Get real visitor IP behind CloudFlare network
+    if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+              $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+              $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+
+$user_ip = getUserIP();
+
+echo $user_ip; // Output IP address [Ex: 177.87.193.134]
+
+
 ?>
