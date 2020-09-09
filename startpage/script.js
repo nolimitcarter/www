a00d6784eb756887309ba421b163045558a86e0b
carterthayer@protonmail.com <carterthayer@protonmail.com>
2020-09-09 15:18:15 -0400

    adding seconds to js script?

--- startpage/script.js
+++ startpage/script.js
@@ -2,6 +2,13 @@ function updateClock() {
     var now = new Date();
 	hours = now.getHours();
 	minutes = now.getMinutes();
+  seconds = now.getSeconds()
+  if (seconds < 10) {
+    time = hours + ':' + "0" + minutes + ':' + "0" + seconds;
+    else {
+      time = hours + ':' + minutes + ':' + seconds;
+    }
+  }
 	if (minutes < 10) {
 		time = hours + ':' + "0" + minutes;
 	} else {
