b672a306294c2624b8084988cddbdf74bd49a597
carterthayer@protonmail.com <carterthayer@protonmail.com>
2020-09-09 15:18:50 -0400

    changed back

--- startpage/script.js
+++ startpage/script.js
@@ -3,12 +3,6 @@ function updateClock() {
 	hours = now.getHours();
 	minutes = now.getMinutes();
   seconds = now.getSeconds()
-  if (seconds < 10) {
-    time = hours + ':' + "0" + minutes + ':' + "0" + seconds;
-    else {
-      time = hours + ':' + minutes + ':' + seconds;
-    }
-  }
 	if (minutes < 10) {
 		time = hours + ':' + "0" + minutes;
 	} else {
